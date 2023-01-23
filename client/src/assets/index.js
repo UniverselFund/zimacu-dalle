bb6030805896f4fb136c68b34313bf1c1b230a91
Karabo Mdluli <kayak.karabo@gmail.com>
2023-01-23 14:27:30 +0200

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import logou from './logo.png';
+
+export {
+  download,
+  logo,
+  preview,
+  logou,
+};
